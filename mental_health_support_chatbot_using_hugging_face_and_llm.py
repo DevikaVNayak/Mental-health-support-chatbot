e715d4385d1b03beb0c644322815b7ca18628190
Devika V Nayak <dvnsirsi@gmail.com>
2025-02-13 09:55:38 +0530

    Update mental_health_support_chatbot_using_hugging_face_and_llm.py

--- mental_health_support_chatbot_using_hugging_face_and_llm.py
+++ mental_health_support_chatbot_using_hugging_face_and_llm.py
@@ -38,7 +38,7 @@ import gradio as gr
 def initialize_llm():
   llm=ChatGroq(
     temperature=0,
-    groq_api_key="",
+    groq_api_key="Add your api key",
     model_name="llama-3.3-70b-versatile"
 )
   return llm
